dd9cdcdb35d56770fa27c75257590588e93024fd
Joao_Nonnenmacker <joaononnen@hotmail.com>
2020-04-07 14:16:50 -0300

    Completed project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,82 @@ app.use(cors());
 
 const repositories = [];
 
+function validateProjectId(request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.'})
+  }
+
+  return next()
+}
+
+app.use('/repositories/:id', validateProjectId)
+
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { id, title, url, techs, like } = request.query
+
+  
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const like = 0
+
+  const repositorie = { id: uuid(), title, url, techs, like}
+
+  repositories.push(repositorie)
+
+  return response.json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const oldRepositorie = repositories.find(repositorie => repositorie.id === id)
+  const like = oldRepositorie.like
+
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    like
+  }
+
+  repositories[repositorieIndex] = repositorie
+
+  return response.json(repositorie)
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  repositories.splice(repositorieIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+  const repositorie = repositories.find(repositorie => repositorie.id === id)
+
+  repositorie.like += 1
+
+  repositories[repositorieIndex] = repositorie
+
+
+  return response.json(repositorie)
+
 });
 
 module.exports = app;
